6a6f47a0f932c310e5ea2387d2ad09ab48bd08df
SimoneSpecchia <simone.specchia26@gmail.com>
2019-06-04 09:41:45 +0200

    Modificato Efi_init

--- GCU_DP11/MDK-ARM/GCU_DP11/efi_init_wrapper.d
+++ GCU_DP11/MDK-ARM/GCU_DP11/efi_init_wrapper.d
@@ -1,3 +1,2 @@
 gcu_dp11\efi_init_wrapper.o: ..\Src\S-Function_Wrappers\Efi_init_wrapper.c
 gcu_dp11\efi_init_wrapper.o: ../Inc/rtwtypes.h
-gcu_dp11\efi_init_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
