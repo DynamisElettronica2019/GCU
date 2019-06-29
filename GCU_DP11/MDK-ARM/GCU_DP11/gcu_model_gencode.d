08ca42f64a7db5138609e091a5617ae75e8cee36
SimoneSpecchia <simone.specchia26@gmail.com>
2019-06-29 16:52:25 +0200

    PID in modalità clamping

--- GCU_DP11/MDK-ARM/GCU_DP11/gcu_model_gencode.d
+++ GCU_DP11/MDK-ARM/GCU_DP11/gcu_model_gencode.d
@@ -1,5 +1,6 @@
 gcu_dp11\gcu_model_gencode.o: ..\Src\GCU_Model_genCode.c
 gcu_dp11\gcu_model_gencode.o: ../Inc/GCU_Model_genCode.h
+gcu_dp11\gcu_model_gencode.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 gcu_dp11\gcu_model_gencode.o: ../Inc/rtwtypes.h
 gcu_dp11\gcu_model_gencode.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 gcu_dp11\gcu_model_gencode.o: ../Inc/timings.h
--- GCU_DP11/MDK-ARM/GCU_DP11/gcu_model_gencode_data.d
+++ GCU_DP11/MDK-ARM/GCU_DP11/gcu_model_gencode_data.d
@@ -1,5 +1,6 @@
 gcu_dp11\gcu_model_gencode_data.o: ..\Src\GCU_Model_genCode_data.c
 gcu_dp11\gcu_model_gencode_data.o: ../Inc/GCU_Model_genCode.h
+gcu_dp11\gcu_model_gencode_data.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 gcu_dp11\gcu_model_gencode_data.o: ../Inc/rtwtypes.h
 gcu_dp11\gcu_model_gencode_data.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 gcu_dp11\gcu_model_gencode_data.o: ../Inc/timings.h
